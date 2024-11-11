e18feb225b1e47bf1082ea0e3dec31690df97449
Pietroo03 <pietro.ponte.03@gmail.com>
2024-11-11 15:25:24 +0100

    import component AppHeader
    add css style

--- react-task-list/src/App.jsx
+++ react-task-list/src/App.jsx
@@ -1,12 +1,13 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Appheader from './components/AppHeader'
 import './App.css'
 
 function App() {
 
   return (
     <>
-
+      <Appheader />
     </>
   )
 }
